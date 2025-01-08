a3f53176f427e009a40acd1157d16f779190b7b2
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-09 06:49:41 +0700

    first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 71, 38, 129),
+      ),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 40),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                side: BorderSide(color: const Color.fromARGB(255, 86, 44, 159), width: 2),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6))),
+            child: Text(
+              'Start Quiz',
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
