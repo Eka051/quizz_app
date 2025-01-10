30bbee778326984b6a7f004cc76404108b85912c
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-10 15:46:10 +0700

    Enhance StartScreen with gradient background and improved layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:quizz_app/start_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: StartScreen()),
+    home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(54, 8, 134, 1),
+                  Color.fromRGBO(107, 8, 255, 16),
+                ],
+              ),
+            ),
+            child: const StartScreen())),
     debugShowCheckedModeBanner: false,
   ));
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,40 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 71, 38, 129),
-      ),
-      child: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+        ),
+        const SizedBox(height: 40),
+        Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
           ),
-          const SizedBox(height: 40),
-          Text(
-            'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-            ),
+        ),
+        const SizedBox(height: 20),
+        OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+              side: BorderSide(
+                  color: const Color.fromARGB(255, 86, 44, 159), width: 2),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6))),
+          child: Text(
+            'Start Quiz',
+            style: TextStyle(color: Colors.white, fontSize: 14),
           ),
-          const SizedBox(height: 20),
-          OutlinedButton(
-            onPressed: () {},
-            style: OutlinedButton.styleFrom(
-                side: BorderSide(color: const Color.fromARGB(255, 86, 44, 159), width: 2),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6))),
-            child: Text(
-              'Start Quiz',
-              style: TextStyle(color: Colors.white, fontSize: 14),
-            ),
-          ),
-        ],
-      )),
-    );
+        ),
+      ],
+    ));
   }
 }
