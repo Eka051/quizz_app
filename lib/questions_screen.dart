877f7cdc48d8d9ce4e49a5f1cb8c6519726f8552
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-12 22:02:28 +0700

    feat: Add answer shuffling to QuizQuestion and update QuestionsScreen to display shuffled answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,25 +18,29 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(answerText: currentQuestion.answers[0], onClick: () {}),
-          AnswerButton(answerText: currentQuestion.answers[1], onClick: () {}),
-          AnswerButton(answerText: currentQuestion.answers[2], onClick: () {}),
-          AnswerButton(answerText: currentQuestion.answers[3], onClick: () {}),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onClick: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
