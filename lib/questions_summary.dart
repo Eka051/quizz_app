54da86d3404bde7a2c2df71abd48faf6a8622a44
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-13 21:04:25 +0700

    refactor: Enhance QuestionsSummary layout with improved styling and scrollable view

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,24 +7,56 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(data['question'] as String),
-                  SizedBox(height: 5),
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
-                ],
-              ),
-            ),
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: data['correct_answer'] == data['user_answer']
+                        ? const Color.fromARGB(255, 7, 116, 206)
+                        : Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.only(right: 10, bottom: 45),
+                  child: Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        data['question'] as String,
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        data['user_answer'] as String,
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 214, 193, 250),
+                        ),
+                      ),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 51, 184, 255),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
